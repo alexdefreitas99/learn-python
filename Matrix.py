cfb692199e9d3296209295a3dfc36e83dfe1cf4c
Alex de Freitas <alex_freitas@sicredi.com.br>
2020-09-19 18:35:05 -0300

    Matrix

---
+++ Matrix.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+# arrayMatrix = np.array([[1, 2, 3], [4, 5, 6]])
+# print("Matriz array: \n", arrayMatrix)
+#
+# print("\n")
+#
+# print("Matriz especifíca ", arrayMatrix[0:1, 0:2])
+
+# print(2 ** 3)
+
+# print("\n")
+#
+# identityMatrix = np.eye(5)
+# print("Matriz identidade: \n", identityMatrix)
+#
+# withAllElemtnsOne = np.ones([4, 4])
+# print("Matriz com 4 colunas e 4 linhas com todos elementos = 1 \n", withAllElemtnsOne)
+#
+# withAllElemtnsOne = np.ones([5, 3])
+# print("Matriz com 3 colunas e 5 linhas com todos elementos = 1 \n", withAllElemtnsOne)
+#
+# withAllElementsZero = np.zeros([6, 5])
+# print("Matriz com 5 colunas e 6 linhas com todos elementos = 0 \n", withAllElementsZero)
+
+
+# for i in np.arange(2, 8, 2):
+#     print(i)
+# print(np.sin(np.pi) + np.sin(np.pi))
+# print(np.pi + np.sin(np.pi))
+# print(np.pi)
+
+
+# def f(x, a=None):
+#     return x + (a)
+#
+#
+# print(f(1))
+
+A = np.array([[1, 3], [3, 1]])
+B = np.array([[2, 1], [3, 1]])
+
+print(type(""))
+
+if type("(A*B)") is str:
+    print("é uma string")
+else:
+    print("Não é uma String")
+
