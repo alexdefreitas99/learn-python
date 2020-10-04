1ee8de6a016e7e2a113797c09df5aa58b2df5356
Alex de Freitas <alex_freitas@sicredi.com.br>
2020-10-04 16:54:35 -0300

    Esquations and functions

---
+++ Functions Newthon-Raphson.py
@@ -0,0 +1,23 @@
+from __future__ import division
+
+
+def derivada(x0): return 3 * x0 ** 2 - 9
+
+
+def funcao(x0): return x0 ** 3 - 9 * x0 + 3
+
+
+def nr(x0, TOL, N):
+    x1 = x0 - funcao(x0) / derivada(x0)
+    i = 1
+    while (abs(funcao(x1)) > TOL) and (i <= N):
+        x0 = x1
+        x1 = x0 - funcao(x0) / derivada(x0)
+        i = i + i
+        if i > N:
+            print("Não houv convergência!")
+        if abs(funcao(x1)) < TOL:
+            return x1
+
+
+nr(0, 10, 20)
